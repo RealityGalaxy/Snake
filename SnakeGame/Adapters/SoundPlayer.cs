a985cc89610a1ba2eff14d2f9c3333127a56c09b
Alanas Švažas <60066251+MrEchox@users.noreply.github.com>
2024-10-31 22:47:52 +0200

    Adapter + sounds (#4)
    Added a SoundLibrary for which I made an adapter and the adapter plays sounds in game

---
+++ SnakeGame/Adapters/SoundPlayer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using SnakeGame.Libraries;
+using SnakeGame.Models;
+
+namespace SnakeGame.Adapters
+{
+    public class SoundPlayer : ISoundPlayer
+    {
+        private readonly SoundManager _soundManager;
+
+        public SoundPlayer()
+        {
+            string soundDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sounds");
+            _soundManager = new SoundManager(soundDirectory);
+        }
+
+        public string PlaySound(string sound)
+        {
+            var soundPath = _soundManager.GetSoundPath(sound);
+            if (soundPath == null)
+            {
+                throw new Exception($"Sound '{sound}' not found.");
+            }
+
+            return soundPath;
+
+        }
+    }
+}
