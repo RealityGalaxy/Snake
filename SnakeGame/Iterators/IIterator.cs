c2daf81e9b84fc5ff19ace213da263223b4cefcb
Perriup <zymogyvis@gmail.com>
2024-11-17 19:44:01 +0200

    -Added an Iterator interface -Made a PointIterator used for Body in Snake class -Made a SnakeIterator used for Snakes in GameInstance class -Made an ObstaclePointIterator for Points in Obstacle class

---
+++ SnakeGame/Iterators/IIterator.cs
@@ -0,0 +1,8 @@
+﻿namespace SnakeGame.Iterators
+{
+    public interface IIterator<T>
+    {
+        bool HasNext();
+        T Next();
+    }
+}
---
+++ SnakeGame/Iterators/PointIterator.cs
@@ -0,0 +1,29 @@
+﻿using SnakeGame.Models;
+
+namespace SnakeGame.Iterators
+{
+    public class PointIterator : IIterator<Point>
+    {
+        private readonly LinkedList<Point> _points;
+        private int _index = 0;
+
+        public PointIterator(LinkedList<Point> points)
+        {
+            _points = points;
+        }
+
+        public bool HasNext()
+        {
+            return _index < _points.Count;
+        }
+
+        public Point Next()
+        {
+            if (!HasNext())
+            {
+                throw new InvalidOperationException("No more elements to iterate over.");
+            }
+            return _points.ElementAt(_index++);
+        }
+    }
+}
